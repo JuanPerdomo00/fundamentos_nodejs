0d3c35145e1895a3b2050aa032973570f2cb81f8
Juan Perdomo - Jakepy <perdomojuan187@gmail.com>
2022-11-11 09:59:09 -0500

    ejemplo de javacript monohilo

---
+++ conceptos/monohilo/monohilo.js
@@ -0,0 +1,21 @@
+console.log("Hola Mundo");
+
+let i = 0;
+
+setInterval(function() {
+  console.log(i);
+  i++;
+   try {
+        if (i === 5) {
+            var a = 3 + z;
+        }
+    } catch (error) {
+        console.error(error)
+    }
+    if(i === 11) {
+        var e = o + 3;
+    }
+  console.log("Estoy aqui");
+}, 1000);
+
+console.log("segunda instruccion");
