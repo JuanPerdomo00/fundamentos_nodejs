eb6d2c6045603134d2b347488ce0b77b2c74173c
Juan Perdomo - Jakepy <perdomojuan187@gmail.com>
2022-11-11 10:01:06 -0500

    ejemplo de callbackHell y como estos son confusos

---
+++ conceptos/asycn/callBackHell.js
@@ -0,0 +1,53 @@
+function hola(nombre, miCallback) {
+    setTimeout(function () {
+        console.log('Hola, '+ nombre);
+        miCallback(nombre);
+    }, 1500);
+}
+
+function hablar(callbackHablar) {
+    setTimeout(function() {
+        console.log('Bla bla bla bla...');
+        callbackHablar();
+    }, 1000);
+}
+
+function adios(nombre, otroCallback) {
+    setTimeout(function() {
+        console.log('Adios', nombre);
+        otroCallback();
+    }, 1000);
+}
+
+//En esta parte del código uso funciones recursivas porque estoy llamando a conversacion dentro de si misma. y mediante un If como estructura de control le digo que cantidad de veces va a  ejectuarse la funcion hablar.
+function conversacion(nombre, veces, callback) {
+    if (veces > 0) {
+        hablar(function () {
+            conversacion(nombre, --veces, callback);
+        })
+    } else {
+        adios(nombre, callback);
+    }
+}
+
+// --
+
+console.log('Iniciando proceso...');
+hola('Aleajandro-sin', function (nombre) {
+    conversacion(nombre, 10, function() {
+        console.log('Proceso terminado');
+    });
+});
+
+/****************HELL**********************/
+// hola('Alejandro', function (nombre) {
+//     hablar(function () {
+//         hablar(function () {
+//             hablar(function () {
+//                 adios(nombre, function() {
+//                     console.log('Terminando proceso...');
+//                 });
+//             });
+//         });
+//     });
+// });
